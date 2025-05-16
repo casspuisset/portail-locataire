035fe71b6abc5031ca01645bb1bc033666604b32
Cassandre Puisset <cpuisset98@gmail.com>
2025-05-16 16:51:35 +0200

    first commit backend

---
+++ src/main/java/com/chatop/portail_locataire/PortailLocataireApplication.java
@@ -0,0 +1,13 @@
+package com.chatop.portail_locataire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailLocataireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailLocataireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatop/portail_locataire/PortailLocataireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.portail_locataire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailLocataireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
